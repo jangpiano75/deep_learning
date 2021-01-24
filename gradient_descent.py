fd922d032f3123ae0e9f9362e4792cc914550b7a
jangpiano75 <hyojung.jang@stonybrook.edu>
2021-01-25 01:07:39 +0900

    Add python file

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,111 +1,126 @@
-
 import numpy as np
-import matplotlib.pyplot as plt
 
-#monomial (단항식)
+#gradient(differentiation)
+def gradient(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)  # x와 형상이 같은 배열을 생성
 
-##데이터 생성
-X = np.random.rand(100)
-pure_y = 6.12 * X+7.05
-noise = np.random.uniform(-0.5, 0.5, 100)
-Y = pure_y + noise
-print(Y)
+    for idx in range(x.size):
+        tmp_val = x[idx]
 
-plt.scatter(X, Y)
-plt.plot(X, pure_y)
+        # f(x+h)
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+
+    return grad
+
+#gradient descent
+def gradient_descent(f, init_x, L = 0.01, I = 20):
+    x = init_x
+
+    for i in range(I):
+        grad = gradient(f,x)
+        x -= L *grad
+
+    return x
+
+
+#Process
+def gradient_descent_process(f, init_x, L = 0.01, I = 100):
+    x = init_x
+    process = []
+
+    for i in range(I):
+        process.append(x.copy())
+        grad = gradient(f,x)
+        x -= L*grad
+
+    return x, np.array(process)
+
+
+#Test
+def function_1(x):
+    return x[0]**2 +x[1]**2
+
+x, x_process = gradient_descent_process(function_1, np.array([-3.0, 4.0]), L = 0.1, I = 50 )
+print(x_process)
+print(x)
+
+from matplotlib.pylab import plt
+
+#graph 1 (2 dimensional graph)
+plt.plot( [-5, 5], [0,0], '-b')    #x axis (color = blue)
+plt.plot( [0,0], [-5, 5], '-b')    #y axis (color = blue)
+plt.plot(x_process[:,0], x_process[:,1], '.-')  # x_process 의 첫번째 열, 두번째 열 circle marker
+
+plt.xlabel("X0")
+plt.ylabel("X1")
 plt.show()
 
-##gradient descent in polynomial
+#graph 2 (contour graph)
+x = np.arange(-5, 5, 0.01)
+y = np.arange(-5, 5, 0.01)
 
-#STEP 1
-#set starting point as 0
+X, Y = np.meshgrid(x, y)
+Z = function_1(np.array([X, Y]))
 
-a = 0
-b = 0
+idx = 1
+plt.subplot(2, 2, idx)
+idx += 1
+plt.plot( x_process[:,0], x_process[:,1], '.-', color="blue")
+plt.contour(X, Y, Z)
 
-#number of observations : n
-n = float(len(X))
+plt.ylim(-5, 5)
+plt.xlim(-5, 5)
 
-L = 0.05  #learning rate
-I = 1500  #number of iterations
-
-#STEP 2
-errors=[ ] 
-for i in range(I):
-
-    Y_pred = a*X + b
-
-    deriv_a = (-2/n)*sum(X*(Y-Y_pred))
-    deriv_b = (-2/n)*sum(Y-Y_pred)
-
-    a = a - L*deriv_a
-    b = b - L*deriv_b
-
-    Q= ((Y_pred - Y)**2).mean()     #((Y-Y_pred)**2).mean()   #(1/n)*sum((Y-Y_pred)**2)
-    errors.append(Q)
-
-print("prediction for parameters: a:{0}, b:{1}, Q:{2}".format(a, b, Q))
-
-
-print(errors) #to show it converges 
- 
-
-
-#STEP 4
-Y_pred = a * X + b
-
-
-plt.scatter(X, Y)
-plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color="red")
-plt.show()
-'''
-
-'''
-#오류가 어떠한 수로 수렴함을 보여줌
-plt.figure(figsize=(10, 10))
-plt.plot(errors)
+plt.plot(0, 0, '+', color = "red")  #극소점
+plt.title("GD")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.show()
 
 
-##polynomial (다항식)
+#graph 3 (3 dimensional graph)
+x, x_process = gradient_descent_process(function_1, np.array([-3.0, 4.0]), L = 0.1, I = 50 )
+
+x = np.arange(-5, 5, 0.01)
+y = np.arange(-5, 5, 0.01)
+x, y = np.meshgrid(x, y)
+z = function_1(np.array([X, Y]))
+
+
+fig = plt.figure()
+ax = fig.gca(projection ="3d")
+surf = ax.plot_wireframe(x, y, z , color='grey', alpha = 0.2)
+ax.plot3D(x_process[:,0], x_process[:,1], color = "blue", alpha = 0.7)
+ax.plot3D(0, 0, '+', color = "red")
+plt.show()
+
+
+#gradient descent to find the parameter
 X_1 = np.random.rand(100)
 X_2 = np.random.rand(100)
 X_3 = np.random.rand(100)
 noise = np.random.uniform(-0.5, 0.5, 100)
 
-pure_y = 3.5*X_1 + 5.9*X_2 + 7*X_3 + 3.7
-Y = pure_y + noise
-print(Y)
 
-##gradient descent in polynomial
-#step1
+def function_1(x):
+    pure_y = 3.5 * X_1 + 5.9 * X_2 + 7 * X_3 + 3.7
+    Y = pure_y + noise
 
-w1=0
-w2=0
-w3=0
-b=0
+    Y_pred = x[0] * X_1 + x[1] * X_2 + x[2] * X_3 + x[3]
+    return (((Y_pred - Y) ** 2).mean())
 
-L = 0.05
-I = 5000
-n = 100
-#STEP 2
-errors=[ ]
-for i in range(I):
 
-    Y_pred = w1*X_1 + w2*X_2 + w3*X_3 + b
+print(gradient_descent(function_1, np.array([0.0, 0.0, 0.0, 0.0]), L=0.05, I=1000))
 
-    deriv_w1 = (-2/n)*sum(X_1*(Y-Y_pred))
-    deriv_w2 = (-2/n)*sum(X_2*(Y-Y_pred))
-    deriv_w3 = (-2/n)*sum(X_3*(Y-Y_pred))
-
-    deriv_b = (-2/n)*sum(Y-Y_pred)
-
-    w1 = w1 - L * deriv_w1
-    w2 = w2 - L * deriv_w2
-    w3 = w3 - L * deriv_w3
-    b = b - L*deriv_b
-
-    Q= ((Y_pred - Y)**2).mean()     #((Y-Y_pred)**2).mean()   #(1/n)*sum((Y-Y_pred)**2)
-    errors.append(Q)
-
-print("prediction for parameters: w1:{0}, w2:{1}, w3:{2}, b:{3}, Q:{4}".format(w1, w2, w3, b, Q))
+x, x_process = gradient_descent_process(function_1, np.array([0.0, 0.0, 0.0, 0.0]), L = 0.05, I = 1000 )
+print(x_process)
+print(x)
